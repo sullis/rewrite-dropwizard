1957861e07403290bfd0892a85f68f7c1fb3ec88
sullis <github@seansullivan.com>
2022-01-18 19:15:57 -0800

    junit 5.8.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -103,9 +103,9 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-reflect")
     testImplementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:latest.release")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.8.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:latest.release")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
 
     testImplementation("org.openrewrite:rewrite-test:${rewriteVersion}")
 
